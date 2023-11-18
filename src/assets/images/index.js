06c86ef873751c581558ff783724ee7c551d310d
sajumulakkal <sajumulakkal@hotmail.com>
2023-11-18 16:27:01 +0800

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,29 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './slabs.png'
+import accenture from './accenture.png'
+import ubs from './ubs.png'
+import creditsuisse from './creditsuisse.png'
+import satyam from './satyam.png'
+import sumtotal from './sumtotal.png'
+import adp from './adp.png'
+import ge from './ge.png'
+import kale from './kale.png'
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    accenture,
+    ubs,
+    creditsuisse,
+    adp,
+    kale,
+    satyam,
+    ge,
+}
---
+++ src/constants/index.js
@@ -0,0 +1,230 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "No SQL",
+        type: "Database",
+    },
+    {
+        imageUrl: express,
+        name: "solidity",
+        type: "Distributed Ledger",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "Oracle",
+        type: "Backend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Production Support Manager for APAC Region",
+        company_name: "Credit Suisse (Part of UBS Group)",
+        icon: "ubs",
+        iconBg: "#accbe1",
+        date: "March 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Solution Architect",
+        company_name: "Accenture",
+        icon: "accenture",
+        iconBg: "#fbc3bc",
+        date: "Nov 2018 - Mar 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Production Support Team Leader",
+        company_name: "Credit-Suisse",
+        icon: "creditsuisse",
+        iconBg: "#b7e4c7",
+        date: "May 2006 - Oct 2018",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "QA Team Lead",
+        company_name: "Sum Total System Pvt Ltd",
+        icon: "sumtotal",
+        iconBg: "#b7e4c7",
+        date: "Dec 2005- Apr 2006",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "System Analyst",
+        company_name: "GE Infrastructure",
+        icon: "ge",
+        iconBg: "#b7e4c7",
+        date: "Dec 2003- Nov 2005",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "QA Team Lead",
+        company_name: "ADP India",
+        icon: "adp",
+        iconBg: "#b7e4c7",
+        date: "Apr 2002- Nov 2003",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Sr Software Engineer",
+        company_name: "Satyam Computer Services Pvt, Ltd",
+        icon: "satyam",
+        iconBg: "#b7e4c7",
+        date: "Nov 1999- Mar 2002",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "System Analyst",
+        company_name: "Kale Consultants Pvt, Ltd",
+        icon: "kale",
+        iconBg: "#b7e4c7",
+        date: "April 1996- Oct 1999",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/sajumulakkal',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://sg.linkedin.com/in/sajumulakkal',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'kitchen Recipes',
+        description: 'Developed a web application using python and flask',
+        link: 'https://github.com/sajumulakkal/my-kitchen-recipes',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Digital Coin Sentiment Tracker',
+        description: ' Website was build using Eitherium & Moralis Platform - Smart Contract is written in Solidity, Contract Events are captured in Moralis server.',
+        link: 'https://github.com/sajumulakkal/slabs-digital-currency-sentiment',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'NFT Card Game',
+        description: 'Using EOS Smart COntract this block chain app was developed.',
+        link: 'https://github.com/sajumulakkal/sLabs_blockchain_kunjali_marakar',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Travel Planning Site ',
+        description: 'App developed in REACT.',
+        link: 'https://github.com/sajumulakkal/sLabs_Travel_Site',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'sLabs- Dapp Token Farm',
+        description: 'Website was build using Eitherium Platform - Smart Contract is written in Solidity.',
+        link: 'https://github.com/sajumulakkal/sLabs-DappTokenFarm',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Burger Ordering App',
+        description: 'Website was build using REACT and FIREBASE.',
+        link: 'https://github.com/sajumulakkal/react-burger',
+    }
+];
